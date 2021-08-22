b1acd9ff78dd51017a493de2c1562db8b5f3516b
Diana <israelyan.diana@mail.ru>
2021-08-22 13:04:55 +0400

    Task solving

---
+++ DnsTestApp/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DnsTestApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DnsTestApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult Search([FromBody] IList<Guid> idClients)
+        {
+            if (!idClients.Any())
+            {
+                return BadRequest();
+            }
+
+            idClients = idClients.Distinct().ToList();
+
+            IDictionary<Guid, IList<Guid>> dictionary =
+                ListToDictionary(new RGDialogsClients().Init());
+
+
+            if (!dictionary.Any())
+            {
+                return Ok(Guid.Empty);
+            }
+
+            Guid result = Guid.Empty;
+
+            bool status = true;
+            foreach (var item in dictionary)
+            {
+                if (item.Value.Count == idClients.Count)
+                {
+                    foreach (var idClient in idClients)
+                    {
+                        if (!item.Value.Contains(idClient))
+                        {
+                            status = false;
+                            break;
+                        }
+                    }
+
+                    if (status)
+                    {
+                        result = item.Key;
+                        break;
+                    }
+                    status = true;
+                }
+            }
+            return Ok(result);
+        }
+
+
+        private IDictionary<Guid, IList<Guid>> ListToDictionary(IList<RGDialogsClients> list)
+        {
+            IDictionary<Guid, IList<Guid>> dictionary = new Dictionary<Guid, IList<Guid>>();
+            foreach (var item in list)
+            {
+                if (dictionary.ContainsKey(item.IDRGDialog))
+                {
+                    IList<Guid> temp = dictionary[item.IDRGDialog];
+                    if (!temp.Contains(item.IDClient))
+                    {
+                        temp.Add(item.IDClient);
+                        dictionary[item.IDRGDialog] = temp;
+                    }
+                }
+                else
+                {
+                    dictionary.Add(
+                        new KeyValuePair<Guid, IList<Guid>>(
+                                item.IDRGDialog, new List<Guid>() { item.IDClient }
+                        ));
+                }
+            }
+            return dictionary;
+        }
+
+    }
+
+}
--- DnsTestApp/Startup.cs
+++ DnsTestApp/Startup.cs
@@ -22,13 +22,12 @@ namespace DnsTestApp
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -40,7 +39,13 @@ namespace DnsTestApp
 
             app.UseRouting();
 
-            app.UseAuthorization();
+
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Test app V1");
+            });
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
